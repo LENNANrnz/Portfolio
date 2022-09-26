4d60ccc1e293a2e41674bdba7ad7a1bec5817628
LENNANrnz <lennanrenzo@gmail.com>
2022-09-26 10:05:44 -0300

    afterrevealfinished

--- body/js/scrollReveal.js
+++ body/js/scrollReveal.js
@@ -3,7 +3,12 @@ var revealArray = [document.querySelector(".article_aboutme"),];
 ScrollReveal().reveal(revealArray, { delay: 375, reset: true,  mobile: true });
 ScrollReveal().reveal(imagem_aboutme, { delay: 375, reset: true,  mobile: true, opacity: 0, distance: '250px', origin: 'left',duration: 2000 });
 
+afterReveal: function myCallback (el) {
+    el.classList.remove('is-animating');
+    el.classList.add('is-visible');
+}
 
+ScrollReveal().reveal({ afterReveal: myCallback });
 
 
 
