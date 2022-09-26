3ba47029d735c6cf58da5f7851b59478a4bd6c50
LENNANrnz <lennanrenzo@gmail.com>
2022-09-26 18:15:01 -0300

    reveal skills add

--- body/js/scrollReveal.js
+++ body/js/scrollReveal.js
@@ -2,9 +2,10 @@ var imagem_aboutme = document.querySelector("#imagem_aboutme");
 var aboutme_paragraph = document.querySelector(".article_aboutme"); 
 var card12Array = [document.getElementById('card1'), document.getElementById('card2')]
 var card34Array = [document.getElementById('card3'), document.getElementById('card4')]
-
+var skills = document.querySelector(".wrapper")
 
 ScrollReveal().reveal(aboutme_paragraph, { delay: 375, reset: true,  mobile: true });
+ScrollReveal().reveal(skills, { delay: 275, reset: true,  mobile: true });
 ScrollReveal().reveal(imagem_aboutme, { delay: 375, reset: true,  mobile: true, opacity: 0, distance: '250px', origin: 'left',duration: 2000 });
 ScrollReveal().reveal(card12Array, { delay: 375, reset: true,  mobile: true, opacity: 0, distance: '250px', origin: 'left',duration: 2000 });
 ScrollReveal().reveal(card34Array, { delay: 375, reset: true,  mobile: true, opacity: 0, distance: '250px', origin: 'right',duration: 2000 });
